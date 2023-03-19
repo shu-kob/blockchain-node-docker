2242bcecfc0949482c5fb5ed1868b3dab5b88b71
小渕周 <shukobuchi@gmail.com>
2023-03-20 00:00:52 +0900

    add astar docker

---
+++ astar/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -gt 0 ]; then VERSION=$1; shift; fi
+if [ $# -gt 0 ]; then echo "syntax: $0 [<VERSION>]"; exit 1; fi
+
+IMAGE_NAME="astar:${VERSION}"
+echo $IMAGE_NAME
+docker build -t $IMAGE_NAME --build-arg VERSION=$VERSION .

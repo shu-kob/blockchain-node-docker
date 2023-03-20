6481a085f39dd67a0cb609316c0d06670b74e05e
小渕周 <shukobuchi@gmail.com>
2023-03-20 23:58:12 +0900

    add bitcoin docker

---
+++ bitcoin/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -gt 0 ]; then COIND_VERSION=$1; shift; fi
+if [ $# -gt 0 ]; then echo "syntax: $0 [<COIND_VERSION>]"; exit 1; fi
+
+IMAGE_NAME="bitcoin:${COIND_VERSION}"
+echo $IMAGE_NAME
+docker build -t $IMAGE_NAME --build-arg COIND_VERSION=$COIND_VERSION .

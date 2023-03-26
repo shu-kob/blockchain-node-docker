9d0f4675451419edbb68164fe2ddf83b33f743f2
小渕周 <shukobuchi@gmail.com>
2023-03-26 12:14:51 +0900

    add docker-compose bitcoin and litecoin

--- litecoin/build.sh
+++ litecoin/build.sh
@@ -3,6 +3,6 @@
 if [ $# -gt 0 ]; then COIND_VERSION=$1; shift; fi
 if [ $# -gt 0 ]; then echo "syntax: $0 [<COIND_VERSION>]"; exit 1; fi
 
-IMAGE_NAME="litecoin:${COIND_VERSION}"
+IMAGE_NAME="litecoin"
 echo $IMAGE_NAME
-docker build -t $IMAGE_NAME --build-arg COIND_VERSION=$COIND_VERSION .
+docker build -t $IMAGE_NAME:${COIND_VERSION} -t $IMAGE_NAME:latest --build-arg COIND_VERSION=$COIND_VERSION .

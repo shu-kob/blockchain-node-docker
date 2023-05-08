cba1c3287c1682c43e14e21b0f8c80b9cc75632a
小渕周 <shukobuchi@gmail.com>
2023-05-08 21:09:39 +0900

    move directory

--- geth/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -gt 0 ]; then BLOCKCHAIN_NODE_VERSION=$1; shift; fi
-if [ $# -gt 0 ]; then echo "syntax: $0 [<VERSION>]"; exit 1; fi
-
-IMAGE_NAME="geth"
-echo $IMAGE_NAME
-docker build -t $IMAGE_NAME:${BLOCKCHAIN_NODE_VERSION} -t $IMAGE_NAME:latest --build-arg VERSION=$BLOCKCHAIN_NODE_VERSION .

ab41ea6937c28d5c943eba6c7197897eab5e86c8
小渕周 <shukobuchi@gmail.com>
2023-05-19 09:04:56 +0900

    refactoring

--- astar/build.sh
+++ astar/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]; then VERSION=$1; shift; fi
-if [ $# -gt 0 ]; then echo "syntax: $0 [<VERSION>]"; exit 1; fi
+if [ $# -gt 0 ]; then BLOCKCHAIN_NODE_VERSION=$1; shift; fi
+if [ $# -gt 0 ]; then echo "syntax: $0 [<BLOCKCHAIN_NODE_VERSION>]"; exit 1; fi
 
-IMAGE_NAME="astar:${VERSION}"
+IMAGE_NAME="astar"
 echo $IMAGE_NAME
-docker build -t $IMAGE_NAME --build-arg VERSION=$VERSION .
+docker build -t $IMAGE_NAME:${BLOCKCHAIN_NODE_VERSION} --build-arg BLOCKCHAIN_NODE_VERSION=$BLOCKCHAIN_NODE_VERSION .
--- polkadot/build.sh
+++ polkadot/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]; then VERSION=$1; shift; fi
-if [ $# -gt 0 ]; then echo "syntax: $0 [<VERSION>]"; exit 1; fi
+if [ $# -gt 0 ]; then BLOCKCHAIN_NODE_VERSION=$1; shift; fi
+if [ $# -gt 0 ]; then echo "syntax: $0 [<BLOCKCHAIN_NODE_VERSION>]"; exit 1; fi
 
 IMAGE_NAME="polkadot"
 echo $IMAGE_NAME
-docker build -t $IMAGE_NAME:${VERSION} -t $IMAGE_NAME:latest --build-arg VERSION=$VERSION .
+docker build -t $IMAGE_NAME:${BLOCKCHAIN_NODE_VERSION} --build-arg BLOCKCHAIN_NODE_VERSION=$BLOCKCHAIN_NODE_VERSION .

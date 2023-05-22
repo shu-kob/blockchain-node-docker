a2356b8c38b3653b27b2e51dcea776f34da2ce01
小渕周 <shukobuchi@gmail.com>
2023-05-22 10:01:22 +0900

    Fix bor build shell

--- polygon/bor/build.sh
+++ polygon/bor/build.sh
@@ -3,6 +3,6 @@
 if [ $# -gt 0 ]; then BLOCKCHAIN_NODE_VERSION=$1; shift; fi
 if [ $# -gt 0 ]; then echo "syntax: $0 [<BLOCKCHAIN_NODE_VERSION>]"; exit 1; fi
 
-IMAGE_NAME="polygon-heimdall"
+IMAGE_NAME="polygon-bor"
 echo $IMAGE_NAME
 docker build -t $IMAGE_NAME:${BLOCKCHAIN_NODE_VERSION} --build-arg BLOCKCHAIN_NODE_VERSION=$BLOCKCHAIN_NODE_VERSION .
